11585be3996c870f0d815859e3ef7d445d53006b
HBFLEX <happybanda@dyuni.ac.mw>
2024-06-07 17:57:07 +0200

    add jobs page

--- jobportal/jobs/urls.py
+++ jobportal/jobs/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import home_page, about_page
+from .views import (
+    home_page, 
+    about_page, 
+    jobs_page
+)
 
 
 
 urlpatterns = [
     path('', home_page, name='home-page'),
     path('about/', about_page, name='about-page'),
+    path('jobs/', jobs_page, name='jobs-page'),
 ]
 
 
--- jobportal/jobs/views.py
+++ jobportal/jobs/views.py
@@ -3,13 +3,16 @@ from .models import Job
 
 
 def home_page(request):
+    return render(request, 'jobs/index.html')
 
+
+def jobs_page(request):
     jobs = Job.objects.all().order_by('-date_created')
     context = { 'jobs': jobs }
 
-    return render(request, 'jobs/index.html', context)
-
+    return render(request, 'jobs/jobs.html', context)
 
 def about_page(request):
-    return render(request, 'jobs/about.html')
+    context = { 'title': 'About Us' }
+    return render(request, 'jobs/about.html', context)
 
