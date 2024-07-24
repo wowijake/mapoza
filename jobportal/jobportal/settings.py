3d145a83b9e318371e7d4b2a98ef9f892fa1070d
HBFLEX <happybanda@dyuni.ac.mw>
2024-07-24 15:52:12 +0200

    feat: add crispy forms

--- jobportal/jobportal/settings.py
+++ jobportal/jobportal/settings.py
@@ -28,7 +28,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobs.apps.JobsConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
